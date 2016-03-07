1f3a4838cb530325b9055eba5fc8b40eeefd5a96
GillesKnockaert <gilles.knockaert@hotmail.com>
2016-03-07 14:02:29 +0100

    CreatingProject
    Setup project on GIT

---
+++ js/feedbackform.js
@@ -0,0 +1,234 @@
+/**
+ * Created by Gilles-Laptop on 3/03/2016.
+ */
+
+
+window.addEventListener("DOMContentLoaded", function () { init() });
+
+//globale vars
+    //Css
+    var csspath = "css/FeedbackStyleV2.css";
+    //path to img folder
+    var img = "img/";
+    //html2canvas library inladen
+    var html2canvaspath = "js/html2canvas.js";
+
+function init() {
+
+    //css inladen
+    loadcss();
+
+    //button send feedback aanmaken
+    createButton();
+
+    //html2canvas inladen
+    loadhtml2canvas();
+
+    //button done highlighting aanmaken
+    readyButton();
+
+
+    document.getElementById("feedbackbtn").onclick = function () {
+
+
+        var myElem = document.getElementById('feedbackmodal');
+        if (myElem === null) {
+
+            //Add overlay div to the html
+            var feedbackmodal = document.createElement("div");
+            feedbackmodal.id = "feedbackmodal";
+            feedbackmodal.className = "feedbackmodal";
+            document.body.appendChild(feedbackmodal);
+            document.getElementById("feedbackmodal").style.visibility = "visible";
+
+        } else {
+            document.getElementById("feedbackmodal").style.visibility = "visible";
+        }
+
+        //Put html code in the feedbackmodal div
+        document.getElementById("feedbackmodal").innerHTML = "" +
+            "<div id='modalcontent'>" +
+            "<div class='modalHeader'>Feedback</div>" +
+            "<form method='post' id='feedbackform' novalidate>" +
+            "<div class='modalContent'>" +
+            "<label for='Subject'>Subject</label>" +
+            "<input type='text' id='Subject' name='Subject' placeholder='Subject' required />" +
+
+            "<label for='Subject'>Description</label>" +
+            "<textarea rows='5' id='Description' name='Description' placeholder='Description' required></textarea>" +
+
+            "<label for='Type'>Type</label>" +
+            "<select id='Type' name='Type'>" +
+            "<option value='Question'>Question</option>" +
+            "<option value='Bug' selected>Problem/Bug</option>" +
+            "<option value='Request'>Request</option>" +
+            "</select>" +
+
+            "<label for='Priority'>Priority</label>" +
+            "<select id='Priority' name='Priority'>" +
+            "<option value='Blocking'>Blocking</option>" +
+            "<option value='Critical'>Critical</option>" +
+            "<option value='High'>High</option>" +
+            "<option value='Medium' selected>Medium</option>" +
+            "<option value='Low'>Low</option>" +
+            "</select>" +
+            "<div id='screenshotbutton' class='button'><img class='icon' src=" + img + "picture.png>Screenshot</div>" +
+            "<div id='highlitebutton' class='button'><img class='icon' src=" + img + "pencil.png>Highlight</div>" +
+            "<div id='screenshotsContainer'></div>" +
+            "</div>" +
+            "<div class='modalFooter'>" +
+            "<div id='closeModal' class='button'>Close</div>" +
+            "<button type='submit' id='submitModal' class='button buttonprimary'>Send</button>" +
+            "</div>" +
+            "</form>" +
+            "</div>";
+
+        //place current body classes in var
+        //add "overflow: hidden;" to the body element
+        bodystate = document.body.className;
+        document.body.className += " bodyoverflowclass";
+
+
+
+
+        document.getElementById("closeModal").onclick = function () {
+            document.getElementById("feedbackmodal").style.visibility = "hidden";
+            document.body.className = bodystate;
+        };
+        document.getElementById("submitModal").onclick = function () {
+            document.getElementById("feedbackmodal").style.visibility = "hidden";
+            document.body.className = bodystate;
+        };
+
+
+
+
+        document.getElementById("screenshotbutton").onclick = function () {
+
+            document.body.className = bodystate;
+
+            document.getElementById("feedbackmodal").style.visibility = "hidden";
+            document.getElementById("feedbackbtn").style.visibility = "hidden";
+
+            //use html2canvas library to generate the html page on a canvas
+            html2canvas(document.body, {
+                onrendered: function(canvas) {
+                    canvas.id = "screenshotcanvasPartial";
+                    var partialContainer = document.createElement("div");
+                    partialContainer.id = "partialContainer";
+                    partialContainer.className = "screenshot";
+                    document.getElementById("screenshotsContainer").appendChild(partialContainer);
+                    var partialoverlay = document.createElement("div");
+                    partialoverlay.id = "partialoverlay";
+                    partialoverlay.className = "partialoverlay";
+                    document.getElementById("partialContainer").appendChild(partialoverlay);
+
+                    document.getElementById("partialoverlay").innerHTML += "<img src=" + img + "pencilWhite.png><p>Click to edit</p>";
+
+                    document.getElementById("partialContainer").innerHTML += "<p>Screenshot from the current view</p>";
+                    document.getElementById("partialContainer").appendChild(canvas);
+
+                    var ctx=canvas.getContext("2d");
+                    var content = ctx.getImageData(0,document.body.scrollTop,canvas.width, document.body.clientHeight);
+                    canvas.height = document.body.clientHeight;
+                    ctx=canvas.getContext("2d");
+                    ctx.putImageData(content, 0, 0);
+                    //-----------partial screenshot in screenshotcontainer--------------------
+                    //document.getElementById("screenshotsContainer").appendChild(canvas);
+                    //var ctx=canvas.getContext("2d");
+                    //var content = ctx.getImageData(0,document.body.scrollTop,canvas.width, document.body.clientHeight);
+                    //canvas.height = document.body.clientHeight;
+                    //ctx=canvas.getContext("2d");
+                    //ctx.putImageData(content, 0, 0);
+                }
+            });
+            html2canvas(document.body, {
+                    onrendered: function(canvas) {
+                        canvas.id = "screenshotcanvasFull";
+                        var fullContainer = document.createElement("div");
+                        fullContainer.id = "fullContainer";
+                        fullContainer.className = "screenshot";
+                        document.getElementById("screenshotsContainer").appendChild(fullContainer);
+
+
+                        var fulloverlay = document.createElement("div");
+                        fulloverlay.id = "fulloverlay";
+                        fulloverlay.className = "fulloverlay";
+                        document.getElementById("fullContainer").appendChild(fulloverlay);
+
+                        document.getElementById("fulloverlay").innerHTML += "<img src=" + img + "pencilWhite.png><p>Click to edit</p>";
+
+                        document.getElementById("fullContainer").innerHTML += "<p>Screenshot from the full website</p>";
+                        document.getElementById("fullContainer").appendChild(canvas);
+
+                        //-----------full screenshot in screenshotcontainer--------------------
+                        //document.getElementById("screenshotsContainer").appendChild(canvas);
+                    }
+                });
+
+            document.body.className += " bodyoverflowclass";
+            document.getElementById("feedbackmodal").style.visibility = "visible";
+            document.getElementById("feedbackbtn").style.visibility = "visible";
+        };
+
+
+        document.getElementById("highlitebutton").onclick = function () {
+            document.body.className = bodystate;
+            document.getElementById("feedbackbtn").style.visibility = "hidden";
+            document.getElementById("feedbackmodal").style.visibility = "hidden";
+            document.getElementById("readyButton").style.visibility = "visible";
+        };
+
+        document.getElementById("readyButton").onclick = function () {
+            document.body.className += " bodyoverflowclass";
+            document.getElementById("feedbackbtn").style.visibility = "visible";
+            document.getElementById("feedbackmodal").style.visibility = "visible";
+            document.getElementById("readyButton").style.visibility = "hidden";
+        };
+
+
+
+    };
+}
+
+
+function loadcss(){
+    var cssId = 'myCss';
+    if (!document.getElementById(cssId))
+    {
+        var head  = document.getElementsByTagName('head')[0];
+        var link  = document.createElement('link');
+        link.id   = cssId;
+        link.rel  = 'stylesheet';
+        link.type = 'text/css';
+        link.href = csspath;
+        link.media = 'all';
+        head.appendChild(link);
+    }
+}
+function loadhtml2canvas() {
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = html2canvaspath;
+    head.appendChild(script);
+    document.body.appendChild(script);
+}
+function createButton(){
+    var feedbackbtn = document.createElement("div");
+    feedbackbtn.id = "feedbackbtn";
+    feedbackbtn.className = "feedbackbtn button";
+    document.body.appendChild(feedbackbtn);
+    feedbackbtn.innerHTML = "<img src=" + img + "Bazookas_Logo_b.png>Send Feedback";
+}
+function readyButton(){
+    var readyButton = document.createElement("div");
+    readyButton.id = "readyButton";
+    readyButton.className = "feedbackbtn button";
+    document.body.appendChild(readyButton);
+    readyButton.innerHTML = "Done Highlighting";
+}
+
+
+
+
