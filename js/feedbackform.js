613f91a7203614ee74edf6c4e2aa378f0330d0ca
GillesKnockaert <gilles.knockaert@hotmail.com>
2016-03-07 14:19:53 +0100

    added backgroundinfo
    added a function that gets all the backgroundinfo

--- js/feedbackform.js
+++ js/feedbackform.js
@@ -27,6 +27,8 @@ function init() {
     //button done highlighting aanmaken
     readyButton();
 
+    getBackgroundinfo();
+
 
     document.getElementById("feedbackbtn").onclick = function () {
 
@@ -227,6 +229,126 @@ function readyButton(){
     readyButton.className = "feedbackbtn button";
     document.body.appendChild(readyButton);
     readyButton.innerHTML = "Done Highlighting";
+}
+
+function getBackgroundinfo(){
+
+
+
+    //-------------Get background information-------------
+
+    var nVer = navigator.appVersion;
+    var nAgt = navigator.userAgent;
+    var browserName = navigator.appName;
+    var fullVersion = '' + parseFloat(navigator.appVersion);
+    var majorVersion = parseInt(navigator.appVersion, 10);
+    var nameOffset, verOffset, ix;
+    var platform = navigator.platform;
+
+    // In Opera 15+, the true version is after "OPR/"
+    if ((verOffset = nAgt.indexOf("OPR/")) != -1) {
+        browserName = "Opera";
+        fullVersion = nAgt.substring(verOffset + 4);
+    }
+    // In older Opera, the true version is after "Opera" or after "Version"
+    else if ((verOffset = nAgt.indexOf("Opera")) != -1) {
+        browserName = "Opera";
+        fullVersion = nAgt.substring(verOffset + 6);
+        if ((verOffset = nAgt.indexOf("Version")) != -1)
+            fullVersion = nAgt.substring(verOffset + 8);
+    }
+    // In MSIE, the true version is after "MSIE" in userAgent
+    else if ((verOffset = nAgt.indexOf("MSIE")) != -1) {
+        browserName = "Microsoft Internet Explorer";
+        fullVersion = nAgt.substring(verOffset + 5);
+    }
+    // In Chrome, the true version is after "Chrome"
+    else if ((verOffset = nAgt.indexOf("Chrome")) != -1) {
+        browserName = "Chrome";
+        fullVersion = nAgt.substring(verOffset + 7);
+    }
+    // In Safari, the true version is after "Safari" or after "Version"
+    else if ((verOffset = nAgt.indexOf("Safari")) != -1) {
+        browserName = "Safari";
+        fullVersion = nAgt.substring(verOffset + 7);
+        if ((verOffset = nAgt.indexOf("Version")) != -1)
+            fullVersion = nAgt.substring(verOffset + 8);
+    }
+    // In Firefox, the true version is after "Firefox"
+    else if ((verOffset = nAgt.indexOf("Firefox")) != -1) {
+        browserName = "Firefox";
+        fullVersion = nAgt.substring(verOffset + 8);
+    }
+    // In most other browsers, "name/version" is at the end of userAgent
+    else if ((nameOffset = nAgt.lastIndexOf(' ') + 1) <
+        (verOffset = nAgt.lastIndexOf('/'))) {
+        browserName = nAgt.substring(nameOffset, verOffset);
+        fullVersion = nAgt.substring(verOffset + 1);
+        if (browserName.toLowerCase() == browserName.toUpperCase()) {
+            browserName = navigator.appName;
+        }
+    }
+    // trim the fullVersion string at semicolon/space if present
+    if ((ix = fullVersion.indexOf(";")) != -1)
+        fullVersion = fullVersion.substring(0, ix);
+    if ((ix = fullVersion.indexOf(" ")) != -1)
+        fullVersion = fullVersion.substring(0, ix);
+
+    majorVersion = parseInt('' + fullVersion, 10);
+    if (isNaN(majorVersion)) {
+        fullVersion = '' + parseFloat(navigator.appVersion);
+        majorVersion = parseInt(navigator.appVersion, 10);
+    }
+
+    var w = window,
+        d = document,
+        e = d.documentElement,
+        g = d.getElementsByTagName('body')[0],
+        x = w.innerWidth || e.clientWidth || g.clientWidth,
+        y = w.innerHeight || e.clientHeight || g.clientHeight;
+
+    var date = new Date();
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+    var dd = date.getDate();
+    var mm = date.getMonth() + 1; //January is 0!
+    var yyyy = date.getFullYear();
+
+    m = checkNumber(m);
+    s = checkNumber(s);
+    dd = checkNumber(dd);
+    mm = checkNumber(mm);
+
+    function checkNumber(i) {
+        if (i < 10) {
+            i = "0" + i;
+        }
+        return i;
+    }
+
+    date = dd + '/' + mm + '/' + yyyy;
+    time = h + ":" + m + ":" + s;
+
+
+    //-------------display background info-------------
+
+    alert(''
+        +'Browser name  = '+browserName+'\n'
+        +'Full version  = '+fullVersion+'\n'
+        +'Major version = '+majorVersion+'\n'
+        +'Browser width = '+x+'\n'
+        +'Browser height = '+y+'\n'
+        +'screen width = '+screen.width+'\n'
+        +'screen height = '+screen.height+'\n'
+        +'Location = '+window.location.href+'\n'
+        +'Date = '+date+'\n'
+        +'Timestamp = '+time+'\n'
+        +'Platform = '+platform+'\n'
+    )
+
+
+
 }
 
 
